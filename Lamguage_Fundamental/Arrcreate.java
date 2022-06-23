1c6e5b6bb28866b5d72838b9742809539585b781
neehhaa <nehawasnik95@outlook.com>
2022-06-23 19:15:57 +0530

    array of array

---
+++ Lamguage_Fundamental/Arrcreate.java
@@ -0,0 +1,12 @@
+//array of array
+
+class Arrcreate{
+    public static void main(String[] args){
+
+        int[][] x = new int[2][];
+        x[0] = new int[2];
+        x[1] = new int[3];
+
+    }
+}
+
