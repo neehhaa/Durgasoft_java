9bd6d38becc4f76641cb4c8583c7555595a5c016
neehhaa <nehawasnik95@outlook.com>
2022-06-24 11:41:18 +0530

    NullPointerException

--- Lamguage_Fundamental/ArrInitial.java
+++ Lamguage_Fundamental/ArrInitial.java
@@ -1,6 +1,6 @@
 class ArrInitial{
     public static void main(String[] args){
-        int[][] x = new int[2][3];
+        int[][] x = new int[2][];
         System.out.println(x);
         System.out.println(x[0]);
         System.out.println(x[0][0]);
