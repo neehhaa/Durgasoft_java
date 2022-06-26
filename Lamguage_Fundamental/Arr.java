6f0187522161bc50b7b52a4a0896494d270b4d2d
neehhaa <nehawasnik95@outlook.com>
2022-06-26 12:35:45 +0530

    length() vs length

--- Lamguage_Fundamental/Arr.java
+++ Lamguage_Fundamental/Arr.java
@@ -1,8 +1,7 @@
 public class Arr  {
     public static void main(String[] args){
-        int x[] = {1,2,3,4};
-        int y[];
-        y = {5,6};
+        int x[] = new int[6];
+        System.out.println(x.length);
 
 
     }
