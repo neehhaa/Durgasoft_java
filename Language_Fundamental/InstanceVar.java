ee97cd93a7225bdce19952686402c9f3dd5b5fe5
neehhaa <nehawasnik95@outlook.com>
2022-06-28 16:25:01 +0530

    using reference call

--- Language_Fundamental/InstanceVar.java
+++ Language_Fundamental/InstanceVar.java
@@ -1,9 +1,13 @@
 public class InstanceVar {
-    int x =10;
-    public static void main(String[] args){
+    int x = 10;
+    int y = 20;
+    public static void main(String[] args) {
 //        System.out.println(x);
         InstanceVar a = new InstanceVar();
         System.out.println(a.x);
-
+        a.m1();
+    }
+    public void m1(){
+        System.out.println(y);
     }
 }
