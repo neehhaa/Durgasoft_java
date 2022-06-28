a34962ffbc21fb766b78fcd79952821d893aba88
neehhaa <nehawasnik95@outlook.com>
2022-06-28 18:53:16 +0530

    Static variables

---
+++ Language_Fundamental/StaticVar.java
@@ -0,0 +1,14 @@
+class StaticVar {
+    static int x = 15;
+    static int y = 20;
+    public static void main(String[] args) {
+        StaticVar v = new StaticVar();
+        System.out.println(v.x);
+        System.out.println(StaticVar.x); //recommended to use class name
+        System.out.println(x); //can use directly
+        v.m1();
+    }
+    public void m1(){
+        System.out.println(y);
+    }
+}
