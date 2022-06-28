da54a864a7622ed49c1859af1d3eca1388d90bcb
neehhaa <nehawasnik95@outlook.com>
2022-06-28 15:26:29 +0530

    using object reference

--- Language_Fundamental/InstanceVar.java
+++ Language_Fundamental/InstanceVar.java
@@ -1,7 +1,9 @@
 public class InstanceVar {
     int x =10;
     public static void main(String[] args){
-        System.out.println(x);
+//        System.out.println(x);
+        InstanceVar a = new InstanceVar();
+        System.out.println(a.x);
 
     }
 }
