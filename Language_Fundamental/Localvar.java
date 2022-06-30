96c01dc1d25e0b2d5e15b34c93166ab361684610
neehhaa <nehawasnik95@outlook.com>
2022-06-30 18:41:42 +0530

    local variable called outside (CTE:cannot find symbol)

---
+++ Language_Fundamental/Localvar.java
@@ -0,0 +1,9 @@
+public class Localvar {
+    public static void main(String[] args){
+        int i = 0;
+        for (int j =0;j<3;j++){
+            i = i+j;
+        }
+        System.out.println(i+"..."+j);
+    }
+}
