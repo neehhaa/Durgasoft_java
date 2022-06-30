9f302289667ef01a5ed41a3216ad2c3ed925ff87
neehhaa <nehawasnik95@outlook.com>
2022-06-30 22:13:32 +0530

    local variable with command line argument

--- Language_Fundamental/LocalVarCLA.java
+++ Language_Fundamental/LocalVarCLA.java
@@ -2,7 +2,10 @@ public class LocalVarCLA {
     public static void main(String[] args){
         int x;
         if(args.length>0){
-            x=10;
+            x = 10;
+        }
+        else {
+            x = 20;
         }
         System.out.println(x);
     }
