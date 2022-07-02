b13e02b7971e8af68ac65c2c232d1d80b8a8be22
neehhaa <nehawasnik95@outlook.com>
2022-07-02 18:58:31 +0530

    method hiding

--- Language_Fundamental/MainParentChild.java
+++ Language_Fundamental/MainParentChild.java
@@ -1,2 +1,10 @@
 public class MainParentChild {
+    public static void main(String[] args){
+        System.out.println("Parent Main");
+    }
+}
+class MainChild extends MainParentChild{
+    public static void main(String[] args){
+        System.out.println("Child Main");
+    }
 }
