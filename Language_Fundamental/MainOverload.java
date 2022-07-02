6f89bbaf4505ee76f4278d5dcc8f613c6772b692
neehhaa <nehawasnik95@outlook.com>
2022-07-02 18:28:54 +0530

    main method overload

---
+++ Language_Fundamental/MainOverload.java
@@ -0,0 +1,8 @@
+public class MainOverload {
+    public static void main(String[] args){
+        System.out.println("String[]");
+    }
+    public static void main(int[] args){
+        System.out.println("int[]");
+    }
+}
