bacea3f2b8d6fdc7bbd8df83df355bea1ae0e74e
neehhaa <nehawasnik95@outlook.com>
2022-07-03 02:29:43 +0530

    CLA CASE 2

---
+++ Language_Fundamental/CLACase2.java
@@ -0,0 +1,9 @@
+public class CLACase2 {
+    public static void main(String[] args) {
+        String[] argh = {"X","Y","Z"};
+        args = argh;
+        for (String s: args) {
+            System.out.println(s);
+        }
+    }
+}
