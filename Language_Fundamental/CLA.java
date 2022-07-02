bddc9c38fb2a41dfe953091729e8b4182eaf71c8
neehhaa <nehawasnik95@outlook.com>
2022-07-03 02:19:50 +0530

    CLA

--- Language_Fundamental/CLA.java
+++ Language_Fundamental/CLA.java
@@ -1,6 +1,7 @@
 public class CLA {
     public static void main(String[] args) {
-        int n = Integer.parseInt(args[0]);
-        System.out.println(n + "..." + (n*n));
+        for (int i=0; i<= args.length;i++){
+            System.out.println(args[i]);
+        }
     }
 }
