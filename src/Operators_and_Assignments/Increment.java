de1106522504f7fae69597eccf7bf458f319e21d
neehhaa <nehawasnik95@outlook.com>
2022-07-05 01:33:56 +0530

    char Increment

--- src/Operators_and_Assignments/Increment.java
+++ src/Operators_and_Assignments/Increment.java
@@ -2,7 +2,7 @@ package Operators_and_Assignments;
 
 public class Increment {
     public static void main(String[] args) {
-        int x = 10;
+        char x = 'a'; //b
         x++;
         System.out.println(x);
     }
