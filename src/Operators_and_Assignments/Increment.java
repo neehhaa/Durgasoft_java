50b0e221ea13b45455dcf3a8a11cf2fd70f7e9eb
neehhaa <nehawasnik95@outlook.com>
2022-07-05 01:43:12 +0530

    Lossy conversion

--- src/Operators_and_Assignments/Increment.java
+++ src/Operators_and_Assignments/Increment.java
@@ -2,9 +2,9 @@ package Operators_and_Assignments;
 
 public class Increment {
     public static void main(String[] args) {
-        boolean x = true; //11.5
-        x++;
-        System.out.println(x);
+        byte b = 10;
+        b = b + 1;
+        System.out.println(b);
     }
 }
 
