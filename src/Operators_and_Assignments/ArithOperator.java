6e74a93186098b417bc59a60023f3d597cd4c349
neehhaa <nehawasnik95@outlook.com>
2022-07-05 03:26:23 +0530

    zero divisions

---
+++ src/Operators_and_Assignments/ArithOperator.java
@@ -0,0 +1,12 @@
+package Operators_and_Assignments;
+
+public class ArithOperator {
+    public static void main(String[] args) {
+       // System.out.println(10/0);  //division by zero
+        System.out.println(10/0.0);  //Infinity
+        System.out.println(-10.0/0); //- Infinity
+      //  System.out.println(0/0);  //division by zero
+        System.out.println(0.0/0);  //NaN
+        System.out.println(-0/0.0);  //NaN
+    }
+}
