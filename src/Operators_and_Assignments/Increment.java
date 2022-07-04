b987c8e50af9aa08957dc6cd58db642a22482195
neehhaa <nehawasnik95@outlook.com>
2022-07-05 01:32:19 +0530

    int Increment

--- src/Operators_and_Assignments/Increment.java
+++ src/Operators_and_Assignments/Increment.java
@@ -2,7 +2,7 @@ package Operators_and_Assignments;
 
 public class Increment {
     public static void main(String[] args) {
-       final int x = 10;
+        int x = 10;
         x++;
         System.out.println(x);
     }
