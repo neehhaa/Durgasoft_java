cf9ae7405300d07b2c6ee9db13b2d75001385a91
neehhaa <nehawasnik95@outlook.com>
2022-07-05 01:35:25 +0530

    Boolean Increment(CE bad operand type)

--- src/Operators_and_Assignments/Increment.java
+++ src/Operators_and_Assignments/Increment.java
@@ -2,7 +2,7 @@ package Operators_and_Assignments;
 
 public class Increment {
     public static void main(String[] args) {
-        double x = 10.5; //11.5
+        boolean x = true; //11.5
         x++;
         System.out.println(x);
     }
