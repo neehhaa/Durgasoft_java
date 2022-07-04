62e7486f493def52d1fb29ea9afef8f5c49a3fad
neehhaa <nehawasnik95@outlook.com>
2022-07-05 00:51:08 +0530

    pre-assignment

---
+++ src/Operators_and_Assignments/Increment.java
@@ -0,0 +1,9 @@
+package Operators_and_Assignments;
+
+public class Increment {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = ++x;
+        System.out.println(y);
+    }
+}
