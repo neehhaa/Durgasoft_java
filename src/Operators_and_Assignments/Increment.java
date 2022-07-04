0d05a63bc32462913328c18f1497f5e233be3c37
neehhaa <nehawasnik95@outlook.com>
2022-07-05 01:03:57 +0530

    pre-increment can be performed only on variables

--- src/Operators_and_Assignments/Increment.java
+++ src/Operators_and_Assignments/Increment.java
@@ -3,7 +3,7 @@ package Operators_and_Assignments;
 public class Increment {
     public static void main(String[] args) {
         int x = 10;
-        int y = ++(++x);
+        int y = ++10;
         System.out.println(y);
     }
 }
