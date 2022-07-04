fd1e2beeb71f27bbff261b72451dfc66c282e020
neehhaa <nehawasnik95@outlook.com>
2022-07-05 01:09:23 +0530

    with final we cant apply Increment or Decrement

--- src/Operators_and_Assignments/Increment.java
+++ src/Operators_and_Assignments/Increment.java
@@ -2,9 +2,9 @@ package Operators_and_Assignments;
 
 public class Increment {
     public static void main(String[] args) {
-        int x = 10;
-        int y = ++10;
-        System.out.println(y);
+       final int x = 10;
+        x++;
+        System.out.println(x);
     }
 }
 
